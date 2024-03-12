74c290288c1cc372aa874d76328fc23ba97490e8
chenjunbai <chenjunbai@fusiontech.cn>
2024-03-12 11:24:14 +0800

    新增检查时间 检查医生等字段信息

--- fxkc-modules/fxkc-peis/src/main/java/org/fxkc/peis/mapper/TjRegBasicProjectMapper.java
+++ fxkc-modules/fxkc-peis/src/main/java/org/fxkc/peis/mapper/TjRegBasicProjectMapper.java
@@ -16,7 +16,8 @@ import java.util.List;
  */
 public interface TjRegBasicProjectMapper extends BaseMapperPlus<TjRegBasicProject, TjRegBasicProjectVo> {
 
-    @Select("select t1.* from tj_reg_basic_project t1 left join tj_basic_project t2 on t1.basic_project_id=t2.id where t1.reg_item_id =#{id} and t1.del_flag='0' order by t2.project_sort ")
+    @Select("select t1.*,t3.check_doctor_name,t3.check_time from tj_reg_basic_project t1 left join tj_basic_project t2 on t1.basic_project_id=t2.id" +
+        " left join  tj_reg_combination_project t3  on t1.reg_item_id = t3.id where t1.reg_item_id =#{id} and t1.del_flag='0' order by t2.project_sort ")
     List<TjRegBasicProjectVo> queryRegBasicProjectList(@Param("id") Long id);
 
 }
